3cbdf7c549a0834d387dbaa1df4ef6b617739d00
Christoph Kettelhoit <ck@christoph-kettelhoit.de>
2012-08-02 18:58:08 +0200

    Implemented minification utilizing the google closure compiler api

--- srv/scriptloader.php
+++ srv/scriptloader.php
@@ -15,8 +15,9 @@ $file = array(
 	'URINameComponent' => '/URI/lib/URINameComponent.js',
 	'URIPathComponent' => '/URI/lib/URIPathComponent.js',
 	'URIKeyValue'      => '/URI/lib/URIKeyValue.js',
-	'URI'              => '/URI/lib/temp.js'
+	'URI'              => '/URI/lib/URI.js'
 );
+
 
 
 
@@ -59,8 +60,53 @@ function resolve(&$token, array &$location, array &$dependent) {
 }
 
 
+function googleClosureCompress(&$string, &$size) {
+	
+	switch ($size) {
+		case 1 :
+			$level = 'WHITESPACE_ONLY';
+			break;
+		case 2 :
+			$level = 'SIMPLE_OPTIMIZATIONS';
+			break;
+		case 3 :
+			$level = 'ADVANCED_OPTIMIZATIONS';
+			break;
+		default : return '';
+	}
+	
+	$content  = 'js_code=' . urlencode($string) . '&';
+	$content .= 'compilation_level=' . $level . '&';
+	$content .= 'output_format=json&';
+	$content .= 'output_info=compiled_code';
+	
+	$connection = array(
+		'http' => array(
+			'method' => 'POST',
+			'content' => $content
+		)
+	);
+	
+	$context  = stream_context_create($connection);
+	$fp       = @ fopen('http://closure-compiler.appspot.com/compile', 'rb', false, $context);
+	
+	if (!$fp) return '';
+	
+	$response = @ stream_get_contents($fp);
+	
+	if ($response == false) return '';
+	
+	$json = json_decode($response);
+	
+	if (!$json->compiledCode) return '';
+	
+	return $json->compiledCode;
+}	
 
-header('Content-Type: text/javascript');
+
+
+
+header('Content-Type: text/javascript; charset=utf-8');
 
 $token = explode(',', $_GET['content']);
 $location = array(); $dependent = array();
@@ -69,6 +115,12 @@ $out = '';
 foreach ($token as $item) resolve($item, $location, $dependent);
 
 foreach ($location as $name) $out .= file_get_contents($_SERVER['DOCUMENT_ROOT'] . $name) . "\n\n\n\n\n";
-	
+
+
+$compress = (int) $_GET['compress'];
+
+if ($compress) $str_cmp = googleClosureCompress($out, $compress);
+if ($str_cmp)  $out = $str_cmp;
+
 print $out;
 ?>
